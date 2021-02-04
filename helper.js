bf8e12e162048ed9c6a6ebad9e60bd2fe752fc60
Pedro Donda <phfdonda@gmail.com>
2021-02-04 09:02:15 -0300

    Add style to form

---
+++ helper.js
@@ -0,0 +1,6 @@
+function buildForm(index){
+  const container = document.querySelector('#container')
+  
+}
+
+module.exports = buildForm
--- server.js
+++ server.js
@@ -7,13 +7,23 @@ const app = express()
 app.set("view engine", "pug")
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
+const jsonPath = __dirname + "/public/data/fields.json"
+const jsonText = fs.readFileSync(jsonPath)
+const defaultText = JSON.parse(jsonText)
+const request_fields = defaultText._embedded.request_fields
+const user_fields = defaultText._embedded.user_fields
+const {buildForm} = require("./helper")
 
 // =============================================================================
 
 // APP GET HOME <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 app.get("/", (req, res) => {
-  res.render("index")
+  res.render("index",{
+    locals:{
+      buildForm
+    }
+  })
 })
 
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
