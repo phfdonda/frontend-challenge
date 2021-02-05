335dff06f18eb4c6fc4c013cbe0a7d28b0264f8b
Pedro Donda <phfdonda@gmail.com>
2021-02-05 16:10:50 -0300

    Implement Next button and Ajax loading.
    Use sessionStorage to persist data of user choices among loading pages

--- public/script.js
+++ public/script.js
@@ -1,27 +1,31 @@
 fetch("data/fields.json")
-  .then((res) => res.json())
+  .then((res) => (res ? res.json() : console.log("No JSON found.")))
   .then((data) => {
     const fields = data._embedded
+    sessionStorage.setItem("step", "0")
     buildForm(fields)
   })
 
 function buildForm(fields) {
-  let step = 0
-  const choicesMade = {}
-  const formContent = selectForm(step, fields)
-  const html = writeHTML(step, formContent)
+  const formContent = selectForm(fields)
+  const html = writeHTML(formContent)
   const form = document.querySelector("#form")
   form.innerHTML = html
+  configButtons(fields)
 }
 
-function writeHTML(step, content) {
+function writeHTML(content) {
+  // This method will draw the form according to the content of the json file
+  const htmlTag = { enumerable: "select", big_text: "textarea" }
+  const field = { enumerable: listOptions(content.values), big_text: "" }
+
   return `
-      <label for=${content.name}>${content.label}</label>
-      <select placeholder="${content.placeholder}">
-        ${listOptions(content.values)}
-      </select>
+      <label for='${content.name}'>${content.label}</label>
+      <${htmlTag[content.type]} class="user-input" placeholder="${
+    content.placeholder
+  }">${field[content.type]}</${htmlTag[content.type]}>
       <span hidden id="warning">Este campo é requerido</span>
-      ${renderButtons(step, content)}
+      ${renderButtons(content)}
     `
 }
 
@@ -38,7 +42,8 @@ function listOptions(values) {
   return optionsHTML
 }
 
-function selectForm(step, fields) {
+function selectForm(fields) {
+  const step = sessionStorage.getItem("step")
   if (step < 5) {
     return fields.request_fields[step]
   } else {
@@ -46,12 +51,54 @@ function selectForm(step, fields) {
   }
 }
 
-function renderButtons(step, content) {
-  const nextText = step == 5 ? "Finalizar" : "Próximo"
+function configButtons(fields) {
+  const step = sessionStorage.getItem('step')
+  const nextBtn = document.getElementById("next")
+  const backBtn = document.getElementById("back")
+
+  nextBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    const inputValue = document.querySelector(".user-input").value
+    if (inputValue.length == 0) {
+      activateWarning()
+    } else {
+      sessionStorage.setItem(step, inputValue)
+      increaseStep()
+      buildForm(fields)
+    }
+    console.log(inputValue)
+  })
+}
+
+function increaseStep() {
+  let step = sessionStorage.getItem("step")
+  step = (parseInt(step) + 1).toString()
+  sessionStorage.setItem("step", step)
+}
+
+function activateWarning() {
+  const warning = document.getElementById("warning")
+  warning.removeAttribute("hidden")
+}
+
+function renderButtons(content) {
+  const step = sessionStorage.getItem("step")
+  const moment = step == 5 ? "final" : "progress"
+  const nextTextMap = {
+    final: {
+      undefined: "Finalizar",
+    },
+    progress: {
+      true: "Próximo",
+      false: "Pular",
+    },
+  }
+  const nextText = nextTextMap[moment][content.required]
+
   return `
   <div class="buttons">
-    <button ${step == 0 ? "hidden" : ""} id="back">Voltar</button>
-    <button id="next">${content.required ? nextText : "Pular"} </button>
+    <button id="back" ${step == 0 ? "hidden" : ""} >Voltar</button>
+    <button id="next">${nextText}</button>
   </div>
   `
 }
--- server.js
+++ server.js
@@ -6,18 +6,13 @@ const app = express()
 app.set("view engine", "pug")
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
-const {buildForm} = require("./helper")
 
 // =============================================================================
 
 // APP GET HOME <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 app.get("/", (req, res) => {
-  res.render("index",{
-    locals:{
-      buildForm
-    }
-  })
+  res.render("index")
 })
 
 // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
